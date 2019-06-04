df92174d5454dcd99bfd1d6f42eb729d0c7e8ced
mjfalahee <mjfalahee@gmail.com>
2019-06-04 13:53:38 -0700

    Added a delete query

--- index.js
+++ index.js
@@ -82,6 +82,30 @@ server.post('/api/zoos', (req, res) => {
   }
 })
 
+
+//remove data
+server.delete('/:id', (req, res) => {
+  db('zoos')
+    .where({id: req.params.id})
+    .del()
+    .then(num => {
+      if (num > 0) {
+        res.status(200).json({
+          message: 'Zoo removed from the database.'
+        });
+      } else {
+        res.status(404).json({
+          message: 'No zoo found with that ID.'
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Error while trying to remove the zoo from the database.'
+      })
+    })
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
